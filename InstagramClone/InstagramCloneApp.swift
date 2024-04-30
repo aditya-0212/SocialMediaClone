0f360628217b6bd7bb645ca2080da91801211ffc
aditya-0212 <adityachoubisa02@gmail.com>
2024-04-30 22:06:14 +0530

    Initial Commit

---
+++ InstagramClone/InstagramCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramCloneApp.swift
+//  InstagramClone
+//
+//  Created by APPLE on 30/04/24.
+//
+
+import SwiftUI
+
+@main
+struct InstagramCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
